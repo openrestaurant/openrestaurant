8686784c73398469031a0d54b8c0917cbe7ff79d
Arshad Chummun <arshad@chummun.com>
2016-04-20 18:29:39 +0400

    Update reset.sh script to work with dev desktop

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 # Script to reset the database and recreate settings.php.
-# Run this script inside /profiles/restaurant/scripts.
+# Run this script inside /profiles/openrestaurant/scripts.
 echo ""
-echo "----------------------------------"
+echo "-----------------------------------------"
 echo "Dropping database..."
-echo "----------------------------------"
+echo "-----------------------------------------"
 drush sql-drop -y
 
 echo ""
-echo "----------------------------------"
-echo "Removing settings.php in sites/default..."
-echo "----------------------------------"
-cd /home/vagrant/sites/openrestaurantv2
+echo "-----------------------------------------"
+echo "Setting permissions for sites/default..."
+echo "-----------------------------------------"
+cd ~/Sites/devdesktop/openrestaurant
 chmod 755 sites/default
+
+echo ""
+echo "-----------------------------------------"
+echo "Removing settings.php in sites/default..."
+echo "-----------------------------------------"
 cd sites/default
-rm -rf settings.php files
-cp default.settings.php settings.php
-mkdir files
-chmod -R 777 settings.php files
+rm -rf settings.php
+
+echo ""
+echo "-----------------------------------------"
+echo "Removing files in sites/default..."
+echo "-----------------------------------------"
+rm -rf files
