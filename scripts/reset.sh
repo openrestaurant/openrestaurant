fd1b81f95bced1ffef3e625fbab04a0a63bfa70d
Arshad Chummun <arshad@chummun.com>
2016-04-23 15:45:29 +0400

    Update path for reset script

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -2,27 +2,35 @@
 # Script to reset the database and recreate settings.php.
 # Run this script inside /profiles/openrestaurant/scripts.
 echo ""
-echo "-----------------------------------------"
+echo "----------------------------------------------"
 echo "Dropping database..."
-echo "-----------------------------------------"
+echo "----------------------------------------------"
 drush sql-drop -y
 
 echo ""
-echo "-----------------------------------------"
+echo "----------------------------------------------"
 echo "Setting permissions for sites/default..."
-echo "-----------------------------------------"
-cd ~/Sites/devdesktop/openrestaurant
+echo "----------------------------------------------"
+cd ../../..
 chmod 755 sites/default
 
 echo ""
-echo "-----------------------------------------"
+echo "----------------------------------------------"
 echo "Removing settings.php in sites/default..."
-echo "-----------------------------------------"
+echo "----------------------------------------------"
 cd sites/default
 rm -rf settings.php
+cp default.settings.php settings.php
 
 echo ""
-echo "-----------------------------------------"
+echo "----------------------------------------------"
 echo "Removing files in sites/default..."
-echo "-----------------------------------------"
+echo "----------------------------------------------"
 rm -rf files
+mkdir files
+
+echo ""
+echo "----------------------------------------------"
+echo "Setting permissions for files and settings.php"
+echo "----------------------------------------------"
+chmod -R 777 files settings.php
