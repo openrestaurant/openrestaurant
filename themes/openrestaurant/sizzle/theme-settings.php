8ad49498aee7a499b1408bf4f64adec73506642b
Arshad Chummun <arshad@chummun.com>
2016-04-30 01:17:31 +0400

    Add layout_width theme settings

--- themes/openrestaurant/sizzle/theme-settings.php
+++ themes/openrestaurant/sizzle/theme-settings.php
@@ -20,6 +20,23 @@ function sizzle_form_system_theme_settings_alter(&$form, FormStateInterface &$fo
   // Hide extra theme settings.
   $form['theme_settings']['#access'] = FALSE;
 
+  // Add layout settings.
+  $form['layout_settings'] = [
+    '#type' => 'details',
+    '#title' => t('Layout settings'),
+    '#open' => TRUE,
+  ];
+
+  $form['layout_settings']['toggle_sizzle_layout_width'] = [
+    '#title' => t('Width'),
+    '#type' => 'radios',
+    '#options' => [
+      'boxed' => t('Boxed'),
+      'full' => t('Full width'),
+    ],
+    '#default_value' => theme_get_setting('features.layout_width') ?? 'boxed',
+  ];
+
   // Add footer settings.
   $form['footer_settings'] = [
     '#type' => 'details',
