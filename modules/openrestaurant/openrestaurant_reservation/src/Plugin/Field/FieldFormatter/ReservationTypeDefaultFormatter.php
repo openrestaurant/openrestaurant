3b95a0db4a77ad7a8ad641e6da36c386ab455157
Arshad Chummun <arshad@chummun.com>
2016-07-21 22:07:25 -0400

    Add a reservation type default formatter

---
+++ modules/openrestaurant/openrestaurant_reservation/src/Plugin/Field/FieldFormatter/ReservationTypeDefaultFormatter.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\openrestaurant_reservation\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'reservation_type_default' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "reservation_type_default",
+ *   label = @Translation("Default"),
+ *   field_types = {
+ *     "reservation_type_field"
+ *   }
+ * )
+ */
+class ReservationTypeDefaultFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return array(
+      // Implement default settings.
+    ) + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return array(
+      // Implement settings form.
+    ) + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $reservation_type = $this->viewValue($item);
+//      $form = entity_get_form_display('reservation', $reservation_type, 'default');
+      $elements[$delta] = array('#markup' => $reservation_type);
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    // The text value has no text format assigned to it, so the user input
+    // should equal the output, including newlines.
+    return nl2br(Html::escape($item->value));
+  }
+
+}
