ccde4fe0113a76461b6ec6cac05ff37fb3917a81
Arshad Chummun <arshad@chummun.com>
2016-07-22 08:18:41 -0400

    Render the reservation type form for the reservation type default formatter

--- modules/openrestaurant/openrestaurant_reservation/src/Plugin/Field/FieldFormatter/ReservationTypeDefaultFormatter.php
+++ modules/openrestaurant/openrestaurant_reservation/src/Plugin/Field/FieldFormatter/ReservationTypeDefaultFormatter.php
@@ -3,10 +3,13 @@
 namespace Drupal\openrestaurant_reservation\Plugin\Field\FieldFormatter;
 
 use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'reservation_type_default' formatter.
@@ -19,7 +22,49 @@ use Drupal\Core\Form\FormStateInterface;
  *   }
  * )
  */
-class ReservationTypeDefaultFormatter extends FormatterBase {
+class ReservationTypeDefaultFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The entity form builder.
+   *
+   * @var \Drupal\Core\Entity\EntityFormBuilderInterface
+   */
+  protected $entityFormBuilder;
+
+
+  /**
+   * @inheritDoc
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, $entityTypeManager, $entityFormBuilder) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+    $this->entityTypeManager = $entityTypeManager;
+    $this->entityFormBuilder = $entityFormBuilder;
+  }
+
+
+  /**
+   * @inheritDoc
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('entity_type.manager'),
+      $container->get('entity.form_builder')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -57,8 +102,17 @@ class ReservationTypeDefaultFormatter extends FormatterBase {
 
     foreach ($items as $delta => $item) {
       $reservation_type = $this->viewValue($item);
-//      $form = entity_get_form_display('reservation', $reservation_type, 'default');
-      $elements[$delta] = array('#markup' => $reservation_type);
+
+      // Get the entity form for the reservation type.
+      $reservation = $this->entityTypeManager->getStorage('reservation')->create(array(
+        'type' => $reservation_type,
+      ));
+      $form = $this->entityFormBuilder->getForm($reservation);
+
+      // Render the form.
+      $elements[$delta] = array(
+        '#markup' => render($form)
+      );
     }
 
     return $elements;
@@ -78,5 +132,4 @@ class ReservationTypeDefaultFormatter extends FormatterBase {
     // should equal the output, including newlines.
     return nl2br(Html::escape($item->value));
   }
-
 }
