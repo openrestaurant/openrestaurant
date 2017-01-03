f03307708e33bc669db037354a6a4be8b0765dc0
Arshad Chummun <arshad@chummun.com>
2017-01-03 14:03:02 +0400

    Add behat and dependencies

---
+++ tests/features/bootstrap/FeatureContext.php
@@ -0,0 +1,21 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\MinkExtension\Context\MinkContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends MinkContext implements Context
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+}
