91c5b144d8d40ec35e453208118a40014bfa5c26
Arshad Chummun <arshad@chummun.com>
2017-01-04 11:52:47 +0400

    Add a iFillInCkeditorFieldWith step

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -1,21 +1,26 @@
 <?php
 
-use Behat\Behat\Context\Context;
-use Behat\MinkExtension\Context\MinkContext;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Mink\Exception\ExpectationException;
+use Drupal\DrupalExtension\Context\DrupalSubContextBase;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext extends MinkContext implements Context
-{
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
-    public function __construct()
-    {
+class FeatureContext extends DrupalSubContextBase implements SnippetAcceptingContext {
+
+  /**
+   * @When I fill in :field ckeditor field with :value
+   */
+  public function iFillInCkeditorFieldWith($field, $value) {
+    $el = $this->getSession()->getPage()->findField($field);
+
+    if (empty($el)) {
+      throw new ExpectationException('Could not find CKEditor field: ' . $field, $this->getSession());
     }
+
+    $fieldId = $el->getAttribute('id');
+    $this->getSession()
+      ->executeScript("CKEDITOR.instances[\"$fieldId\"].setData(\"$value\");");
+  }
 }
