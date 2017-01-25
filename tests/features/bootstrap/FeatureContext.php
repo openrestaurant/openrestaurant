776be9de036563fddcb082dd5fbeb992770b4d3c
Arshad Chummun <arshad@chummun.com>
2017-01-25 23:25:54 +0400

    Extend RawDrupalContext instead of DrupalSubContextBase

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -2,12 +2,12 @@
 
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Mink\Exception\ExpectationException;
-use Drupal\DrupalExtension\Context\DrupalSubContextBase;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext extends DrupalSubContextBase implements SnippetAcceptingContext {
+class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
 
   /**
    * @When I fill in :field ckeditor field with :value
